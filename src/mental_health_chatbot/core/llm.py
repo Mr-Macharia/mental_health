59db19fa6da727a76c86ec2121c879160bd3cb16
Mr-Macharia <gichogumacharia001@gmail.com>
2026-07-04 23:33:30 +0300

    feat: add Excalidraw design file and update LLM integration tests

--- src/mental_health_chatbot/core/llm.py
+++ src/mental_health_chatbot/core/llm.py
@@ -1,14 +1,30 @@
 import os
-from agno.models.openai.like import OpenAILike
+# from agno.models.openai.like import OpenAILike
+# from agno.models.deepseek import DeepSeek
+from agno.models.deepinfra import DeepInfra
+# from agno.models.fireworks import Fireworks
 from dotenv import load_dotenv
 
 load_dotenv()
 
 api_key = os.getenv("MODEL_ACCESS_KEY")
 
-default_model=OpenAILike(
-    id="gemma-4-31B-it", # openai-gpt-oss-20b
-    api_key=api_key,
-    base_url="https://inference.do-ai.run/v1/",
+# default_model=OpenAILike(
+#     id="gemma-4-31B-it", # openai-gpt-oss-20b
+#     api_key=api_key,
+#     base_url="https://inference.do-ai.run/v1/",
+# )
+
+
+# default_model=Fireworks(
+#     id="accounts/fireworks/models/qwen3p7-plus"
+# )
+
+default_model=DeepInfra(
+    id="google/gemma-4-31B-it"
 )
 
+
+# default_model=DeepSeek(
+#     id="deepseek-chat"
+# )
--- tests/integration/llm_test.py
+++ tests/integration/llm_test.py
@@ -1,21 +1,15 @@
-import os
+# import os
 from agno.agent import Agent
-from agno.models.openai.like import OpenAILike
+# from agno.models.openai.like import OpenAILike
+from mental_health_chatbot.core.llm import default_model
 from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.getenv("MODEL_ACCESS_KEY")
-
-ai_model=OpenAILike(
-    id="openai-gpt-oss-20b", # openai-gpt-oss-20b
-    api_key=api_key,
-    base_url="https://inference.do-ai.run/v1/",
-)
 
 agent = Agent(
     name="Test Agent",
-    model=ai_model,
+    model=default_model,
     introduction="If run is successful return response",
 )
 
